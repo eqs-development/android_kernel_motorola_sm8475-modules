OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Get RCPI value in cfg80211 get_station callback
Currently, API to get RCPI value is wlan_hdd_get_rcpi(),
invoke this API to log computed RCPI.

Change-Id: I99e137e37a0c2c4a94f37009b6c00d992c440ec6
CRs-Fixed: 1115232

core/hdd/src/wlan_hdd_stats.c
@@ -1769,6 +1769,7 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 
 	uint32_t vht_mcs_map;
 	enum eDataRate11ACMaxMcs vhtMaxMcs;
+	int32_t rcpi_value;
 
 	ENTER_DEV(dev);
 
@@ -1806,6 +1807,10 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 
 	if (0 != status)
 		return status;
+
+	if (pHddCtx->rcpi_enabled)
+		wlan_hdd_get_rcpi(pAdapter, (uint8_t *)mac, &rcpi_value,
+				  RCPI_MEASUREMENT_TYPE_AVG_MGMT);
 
 	wlan_hdd_get_station_stats(pAdapter);
 	sinfo->signal = pAdapter->hdd_stats.summary_stat.rssi;

eqs-development/android_kernel_motorola_sm8475-modules

qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_stats.c
@@ -3920,6 +3920,7 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 
 	uint32_t vht_mcs_map;
 	enum data_rate_11ac_max_mcs vht_max_mcs;
+	int32_t rcpi_value;
 
 	ENTER_DEV(dev);
 
@@ -3959,6 +3960,10 @@ static int __wlan_hdd_cfg80211_get_station(struct wiphy *wiphy,
 		wlan_hdd_fill_station_info_signal(sinfo);
 		return 0;
 	}
+
+	if (hdd_ctx->rcpi_enabled)
+		wlan_hdd_get_rcpi(adapter, (uint8_t *)mac, &rcpi_value,
+				  RCPI_MEASUREMENT_TYPE_AVG_MGMT);
 
 	wlan_hdd_get_station_stats(adapter);
 
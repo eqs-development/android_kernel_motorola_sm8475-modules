625a37aeca5c3420f80a980f0efa980997d5493c
shuoyan <shuoyan@motorola.com>
2018-11-11 11:31:13 -0600

    disp: msm: Enable "val" to control HBM property
    Enable "val" to update HBM property, so user-mode can control
    the enable/disable of HBM property through connector property.
    
    Port form 4ec72b4
    
    Change-Id: Ieed101ff0bab707ebf2dabc7e533564bf60cbc90
    Signed-off-by: shuoyan <shuoyan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697582
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/sde/sde_connector.c
+++ qcom/opensource/display-drivers/msm/sde/sde_connector.c
@@ -1819,8 +1819,7 @@ static int sde_connector_atomic_set_property(struct drm_connector *connector,
 			SDE_ERROR_CONN(c_conn, "dynamic bit clock set failed, rc: %d", rc);
 
 	case CONNECTOR_PROP_HBM:
-		param_info.value = sde_connector_get_property(
-				connector->state, CONNECTOR_PROP_HBM);
+		param_info.value = val;
 		param_info.param_idx = PARAM_HBM_ID;
 		param_info.param_conn_idx = CONNECTOR_PROP_HBM;
 		rc = _sde_connector_update_param(c_conn, &param_info);
@@ -1828,8 +1827,7 @@ static int sde_connector_atomic_set_property(struct drm_connector *connector,
 			goto end;
 		break;
 	case CONNECTOR_PROP_ACL:
-		param_info.value = sde_connector_get_property(
-				connector->state, CONNECTOR_PROP_ACL);
+		param_info.value = val;
 		param_info.param_idx = PARAM_ACL_ID;
 		param_info.param_conn_idx = CONNECTOR_PROP_ACL;
 		rc = _sde_connector_update_param(c_conn, &param_info);

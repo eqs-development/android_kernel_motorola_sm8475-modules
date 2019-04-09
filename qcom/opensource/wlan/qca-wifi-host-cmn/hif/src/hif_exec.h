arter97/caf_qcacld-3.0

qcacmn: HIF schedule latency measurement
Add support to measure interrupt latencies, store them
in different "bins" and display the constructed histogram
through user command.

Change-Id: I268be6b39745d550ac7db5d321c2d1fdb2d17d41
CRs-Fixed: 2365812

hif/src/hif_exec.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2018 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -20,7 +20,23 @@
 #define __HIF_EXEC_H__
 
 #include <hif.h>
+/*Number of buckets for latency*/
+#define HIF_SCHED_LATENCY_BUCKETS 8
 
+/*Buckets for latency between 0 to 2 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_0_2 2
+/*Buckets for latency between 3 to 10 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_3_10 10
+/*Buckets for latency between 11 to 20 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_11_20 20
+/*Buckets for latency between 21 to 50 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_21_50 50
+/*Buckets for latency between 50 to 100 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_51_100 100
+/*Buckets for latency between 100 to 250 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_101_250 250
+/*Buckets for latency between 250 to 500 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_251_500 500
 
 struct hif_exec_context;
 
@@ -46,6 +62,8 @@ struct hif_execution_ops {
  *	determine if this context should reschedule or wait for an interrupt.
  *	This function may be used as a hook for post processing.
  *
+ * @sched_latency_stats: schdule latency stats for different latency buckets
+ * @tstamp: timestamp when napi poll happens
  * @irq_disable: called before scheduling the context.
  * @irq_enable: called when the context leaves polling mode
  */
@@ -64,6 +82,9 @@ struct hif_exec_context {
 	bool (*work_complete)(struct hif_exec_context *, int work_done);
 	void (*irq_enable)(struct hif_exec_context *);
 	void (*irq_disable)(struct hif_exec_context *);
+	const char* (*irq_name)(int irq_no);
+	int64_t sched_latency_stats[HIF_SCHED_LATENCY_BUCKETS];
+	int64_t tstamp;
 
 	uint8_t cpu;
 	struct qca_napi_stat stats[NR_CPUS];

eqs-development/android_kernel_motorola_sm8475-modules

qcom/opensource/wlan/qca-wifi-host-cmn/hif/src/hif_exec.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2019 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -21,6 +21,23 @@
 
 #include <hif.h>
 #include <linux/cpumask.h>
+/*Number of buckets for latency*/
+#define HIF_SCHED_LATENCY_BUCKETS 8
+
+/*Buckets for latency between 0 to 2 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_0_2 2
+/*Buckets for latency between 3 to 10 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_3_10 10
+/*Buckets for latency between 11 to 20 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_11_20 20
+/*Buckets for latency between 21 to 50 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_21_50 50
+/*Buckets for latency between 50 to 100 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_51_100 100
+/*Buckets for latency between 100 to 250 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_101_250 250
+/*Buckets for latency between 250 to 500 ms*/
+#define HIF_SCHED_LATENCY_BUCKET_251_500 500
 
 struct hif_exec_context;
 
@@ -46,8 +63,11 @@ struct hif_execution_ops {
  *	determine if this context should reschedule or wait for an interrupt.
  *	This function may be used as a hook for post processing.
  *
+ * @sched_latency_stats: schdule latency stats for different latency buckets
+ * @tstamp: timestamp when napi poll happens
  * @irq_disable: called before scheduling the context.
  * @irq_enable: called when the context leaves polling mode
+ * @irq_name: pointer to function to return irq name/string mapped to irq number
  */
 struct hif_exec_context {
 	struct hif_execution_ops *sched_ops;
@@ -65,6 +85,9 @@ struct hif_exec_context {
 	bool (*work_complete)(struct hif_exec_context *, int work_done);
 	void (*irq_enable)(struct hif_exec_context *);
 	void (*irq_disable)(struct hif_exec_context *);
+	const char* (*irq_name)(int irq_no);
+	uint64_t sched_latency_stats[HIF_SCHED_LATENCY_BUCKETS];
+	uint64_t tstamp;
 
 	uint8_t cpu;
 	struct qca_napi_stat stats[NR_CPUS];
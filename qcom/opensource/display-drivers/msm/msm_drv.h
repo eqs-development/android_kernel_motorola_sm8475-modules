c5a831dfbfd5176b1c83f571f0b9f99932b2cbf6
daiss1 <daiss1@lenovo.com>
2021-05-10 11:14:30 +0800

    drm: Add the DC panel param feature
    Add the DC panel param feature which will be controlled
    from the user-space
    
    Change-Id: Ia241893614f91ba209ce830db033033235da2dfc
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1943795
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    
    DC mode: add return value
    
    There is no return value for DC mode setting.When DC mode failed to be set,
    the proper value can not be saved on Moto Display Panel service, that caused
    "do nothing" for next try which then caused in valid DC mode after phone
    resume.
    
    Change-Id: I8ecf5fe9c3a96ac95d026d9b550910f040f18722
    Reviewed-on: https://gerrit.mot.com/2385255
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Submit-Approved: Jira Key
    
    dsi_panel: panel_feature: corret the code mistake
    
        Without "break", After Setting CABC, then also set dc.
        It is mistake.
    
    Change-Id: I8c1859f8c4a38a9104ce278d3f29454193c5b4a2
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2416895
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/msm_drv.h
+++ qcom/opensource/display-drivers/msm/msm_drv.h
@@ -240,6 +240,7 @@ enum msm_mdp_conn_property {
 	CONNECTOR_PROP_HBM,
 	CONNECTOR_PROP_CABC,
 	CONNECTOR_PROP_ACL,
+	CONNECTOR_PROP_DC,
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
 };
@@ -430,6 +431,7 @@ enum msm_param_id {
 	PARAM_HBM_ID = 0,
 	PARAM_CABC_ID,
 	PARAM_ACL_ID,
+	PARAM_DC_ID,
 	PARAM_ID_NUM
 };
 

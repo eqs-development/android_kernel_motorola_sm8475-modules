8fb0458d6fb82c8636ffd49b62f80279431d04e6
dudd1 <dudd1@motorola.com>
2021-06-30 19:44:11 +0800

    lpass-cdc: update channel count rule
    if channel count is already 0, do not decrease again, it will cause
    graph_prepare err.
    
    Change-Id: I6c4fc1447b294159635dbfccecffe31dcd9e6a24
    Reviewed-on: https://gerrit.mot.com/1997923
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yuesheng Huang <huangys@motorola.com>
    Submit-Approved: Dandan Du <dudd1@motorola.com>

--- qcom/opensource/audio-kernel/asoc/codecs/lpass-cdc/lpass-cdc-tx-macro.c
+++ qcom/opensource/audio-kernel/asoc/codecs/lpass-cdc/lpass-cdc-tx-macro.c
@@ -611,11 +611,15 @@ static int lpass_cdc_tx_macro_tx_mixer_put(struct snd_kcontrol *kcontrol,
 		return -EINVAL;
 
 	if (enable) {
-		set_bit(dec_id, &tx_priv->active_ch_mask[dai_id]);
-		tx_priv->active_ch_cnt[dai_id]++;
+		if (!(test_bit(dec_id, &tx_priv->active_ch_mask[dai_id]))) {
+			set_bit(dec_id, &tx_priv->active_ch_mask[dai_id]);
+			tx_priv->active_ch_cnt[dai_id]++;
+		}
 	} else {
-		tx_priv->active_ch_cnt[dai_id]--;
-		clear_bit(dec_id, &tx_priv->active_ch_mask[dai_id]);
+		if (test_bit(dec_id, &tx_priv->active_ch_mask[dai_id])) {
+			tx_priv->active_ch_cnt[dai_id]--;
+			clear_bit(dec_id, &tx_priv->active_ch_mask[dai_id]);
+		}
 	}
 	snd_soc_dapm_mixer_update_power(widget->dapm, kcontrol, enable, update);
 

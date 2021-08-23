686168f060e409109a29c9177a48e219d3f36639
zhanggb <zhanggb@motorola.com>
2021-08-23 15:53:03 +0800

    Kernel: Add Moto extern display code
    [1] read panel mipi command(change to sys/class/drm)
    Use below command to get current dsi panel command:
    adb shell "cat /sys/class/drm/card0-DSI-1/dsi_display_parse_para"
    
    [2]Debug tool - parse/update lcd para from data file
    Add a parse function to get lcd bringup parameters from file in
    /vendor/firmware/ OR /data/vendor/param/display/. This help vendor
    engineer to debug lcd issue without requiring moto engineer to
    bulid new dtbo or boot image, it is also used by ParamUpdater app.
      1.Please push lcd para file lcd-paras.txt to /vendor/firmware/
        or /data/vendor/param/display/;
      2.Below command can update lcd paras:
        adb shell "echo num > /sys/class/drm/card0-DSI-1/dsi_display_parse_para"
    
    Progagate from below changeID:
    Ifefe7706b3f5d9c6f6aa11e26282e94a053b1ae4
    I6483b06b1be2bd51a5e8f49b72a476f5f19f0275
    Icc6578f4cade1acbd3ea09076128fca397600a89
    
    Change-Id: Ib90d4a6af8c34a55d9afd0688f995777c8c32c96
    Signed-off-by: zhanggb <zhanggb@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2048596
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -31,6 +31,7 @@
 #include "sde_dsc_helper.h"
 #include "sde_vdc_helper.h"
 #include "dsi_display.h"
+#include "dsi_display_mot_ext.h"
 
 #if defined(CONFIG_DRM_DYNAMIC_REFRESH_RATE)
 static struct blocking_notifier_head dsi_freq_head =
@@ -4313,6 +4314,18 @@ static int dsi_panel_parse_mot_panel_config(struct dsi_panel *panel,
 
 	panel->tp_state_check_enable = of_property_read_bool(of_node,
 				"qcom,tp_state_check_enable");
+
+
+       rc = of_property_read_u32(of_node,
+                       "qcom,mdss-dsi-panel-param-verision",
+                       &panel->paramVersion);
+       if (rc) {
+           DSI_INFO("qcom,mdss-dsi-panel-param-verision not found, set param version to max value 991231\n");
+           panel->paramVersion = 991231;//format:YYMMDD
+       } else {
+           DSI_INFO("got paramVersion %d from qcom,mdss-dsi-panel-param-verision\n", panel->paramVersion);
+       }
+
 	return rc;
 }
 

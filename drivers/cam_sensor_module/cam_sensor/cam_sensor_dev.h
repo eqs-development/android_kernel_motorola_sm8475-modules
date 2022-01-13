44b7ba552a46620370cf054732aac5416d844732
Ravi Vembu <raviv@motorola.com>
2022-01-13 13:51:19 -0600

    cam-sensor: Wrap addr switch inside a Macro
    Wrap the addr switch inside a Macro that is turned on only for products
    that require it, since this breaks other products that dont have addr
    switch implemented in the HAL.
    
    Change-Id: Iff24baf6dba5c2dc8d12bc44142b23a4e8cbea7c
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Mot-CRs-Fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/2169964
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jian Luan <jamesluan@motorola.com>
    Submit-Approved: Jira Key

--- drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
+++ drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.h
@@ -139,6 +139,7 @@ struct cam_sensor_ctrl_t {
 		CAM_SENSOR_NAME_MAX_SIZE];
 	bool                           is_aon_user;
 	bool                           hw_no_ops;
+#ifdef CONFIG_CCI_ADDR_SWITCH
 	/* add i2c addr switch support*/
 	uint8_t                        i2c_addr_switch;
 	uint16_t                       second_i2c_address;
@@ -147,6 +148,7 @@ struct cam_sensor_ctrl_t {
 	uint16_t                       i2c_switch_reg_addr;
 	uint16_t                       i2c_switch_reg_data;
 	uint16_t                       i2c_switch_reg_delayMs;
+#endif
 };
 
 /**

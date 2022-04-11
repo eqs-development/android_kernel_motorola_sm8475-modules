5a23ab1c0393abc667ac9d5576a9d00244da6d82
wangshuo18 <wangshuo18@lenovo.com>
2022-04-11 14:34:40 +0800

    camera-kernel: eqs: enbale CAM_SENSOR_PROBE_DEBUG
    detail:
    enable CAM_SENSOR_PROBE_DEBUG for frontier.
    try to repeat probe when meet probe failed issue.
    
    Change-Id: I99570fbe4c9022971650bae917a6f97fc984a3de
    Signed-off-by: wangshuo18 <wangshuo18@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2238338
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xingyun Fu <fuxy7@motorola.com>
    Reviewed-by: Bolei Shang <shangbl1@lenovo.com>
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Zilong Zhang <zhangzl7@lenovo.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/camera-kernel/config/eqs.mk
+++ qcom/opensource/camera-kernel/config/eqs.mk
@@ -3,6 +3,8 @@
 # Localized KCONFIG settings
 # MMI_STOPSHIP Camera: Remove for user build
 CONFIG_CCI_DEBUG_INTF := y
+CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
+ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1

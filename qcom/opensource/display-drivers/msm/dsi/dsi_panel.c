f26dfd9a1db947d0fb53916b585a12815ed24281
daiss1 <daiss1@lenovo.com>
2022-05-16 20:32:17 +0800

    msm/display: Add vci and vio gpio control
    Add vci and vio gpio control
    vci and vio need enable/disable gpio to control
    
    Change-Id: I1f79cf3ff3d045181ff823cb29c0ab809742e2e2
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2270133
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -256,6 +256,21 @@ static int dsi_panel_gpio_request(struct dsi_panel *panel)
 		}
 	}
 
+	if (gpio_is_valid(r_config->vci_en_gpio)) {
+		rc = gpio_request(r_config->vci_en_gpio, "vci_en_gpio");
+		if (rc) {
+			DSI_ERR("request for vci_en_gpio failed, rc=%d\n", rc);
+			//goto error_release_vci;
+		}
+	}
+	if (gpio_is_valid(r_config->vio_en_gpio)) {
+		rc = gpio_request(r_config->vio_en_gpio, "vio_en_gpio");
+		if (rc) {
+			DSI_ERR("request for vio_en_gpio failed, rc=%d\n", rc);
+			//goto error_release_vio;
+		}
+	}
+
 	if (gpio_is_valid(panel->panel_test_gpio)) {
 		rc = gpio_request(panel->panel_test_gpio, "panel_test_gpio");
 		if (rc) {
@@ -296,6 +311,12 @@ static int dsi_panel_gpio_release(struct dsi_panel *panel)
 
 	if (gpio_is_valid(panel->reset_config.lcd_mode_sel_gpio))
 		gpio_free(panel->reset_config.lcd_mode_sel_gpio);
+
+	if (gpio_is_valid(panel->reset_config.vio_en_gpio))
+		gpio_free(panel->reset_config.vio_en_gpio);
+
+	if (gpio_is_valid(panel->reset_config.vci_en_gpio))
+		gpio_free(panel->reset_config.vci_en_gpio);
 
 	if (gpio_is_valid(panel->panel_test_gpio))
 		gpio_free(panel->panel_test_gpio);
@@ -496,6 +517,9 @@ static int dsi_panel_power_on(struct dsi_panel *panel)
 		pr_info("%s: (%s)+power is alway on \n", __func__, panel->name);
 		goto exit;
 	}
+	if (gpio_is_valid(panel->reset_config.vio_en_gpio))
+		gpio_set_value(panel->reset_config.vio_en_gpio, 1);
+
 	pr_info("[drm] dsi_pwr_enable_regulator\n");
 	rc = dsi_pwr_enable_regulator(&panel->power_info, true);
 	if (rc) {
@@ -503,6 +527,10 @@ static int dsi_panel_power_on(struct dsi_panel *panel)
 				panel->name, rc);
 		goto exit;
 	}
+
+	if (gpio_is_valid(panel->reset_config.vci_en_gpio))
+		gpio_set_value(panel->reset_config.vci_en_gpio, 1);
+
 	pr_info("[drm] dsi_panel_set_pinctrl_state\n");
 	rc = dsi_panel_set_pinctrl_state(panel, true);
 	if (rc) {
@@ -524,6 +552,12 @@ error_disable_gpio:
 
 	if (gpio_is_valid(panel->bl_config.en_gpio))
 		gpio_set_value(panel->bl_config.en_gpio, 0);
+
+	if (gpio_is_valid(panel->reset_config.vio_en_gpio))
+		gpio_set_value(panel->reset_config.vio_en_gpio, 0);
+
+	if (gpio_is_valid(panel->reset_config.vci_en_gpio))
+		gpio_set_value(panel->reset_config.vci_en_gpio, 0);
 
 	(void)dsi_panel_set_pinctrl_state(panel, false);
 
@@ -570,10 +604,16 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 		       rc);
 	}
 
+	if (gpio_is_valid(panel->reset_config.vio_en_gpio))
+		gpio_set_value(panel->reset_config.vio_en_gpio, 0);
+
 	rc = dsi_pwr_enable_regulator(&panel->power_info, false);
 	if (rc)
 		DSI_ERR("[%s] failed to enable vregs, rc=%d\n",
 				panel->name, rc);
+
+	if (gpio_is_valid(panel->reset_config.vci_en_gpio))
+		gpio_set_value(panel->reset_config.vci_en_gpio, 0);
 
 exit:
 	return rc;
@@ -2991,13 +3031,18 @@ static int dsi_panel_parse_gpios(struct dsi_panel *panel)
 	const char *data;
 	struct dsi_parser_utils *utils = &panel->utils;
 	char *reset_gpio_name, *mode_set_gpio_name;
+	char *vio_en_gpio_name, *vci_en_gpio_name;
 
 	if (!strcmp(panel->type, "primary")) {
 		reset_gpio_name = "qcom,platform-reset-gpio";
 		mode_set_gpio_name = "qcom,panel-mode-gpio";
+		vio_en_gpio_name = "qcom,platform-vio-enable-gpio";
+		vci_en_gpio_name = "qcom,platform-vci-enable-gpio";
 	} else {
 		reset_gpio_name = "qcom,platform-sec-reset-gpio";
 		mode_set_gpio_name = "qcom,panel-sec-mode-gpio";
+		vio_en_gpio_name = "qcom,platform-sec-vio-enable-gpio";
+		vci_en_gpio_name = "qcom,platform-sec-vci-enable-gpio";
 	}
 
 	panel->reset_config.reset_gpio = utils->get_named_gpio(utils->data,
@@ -3029,6 +3074,16 @@ static int dsi_panel_parse_gpios(struct dsi_panel *panel)
 		DSI_DEBUG("mode gpio not specified\n");
 
 	DSI_DEBUG("mode gpio=%d\n", panel->reset_config.lcd_mode_sel_gpio);
+
+	panel->reset_config.vio_en_gpio = utils->get_named_gpio(
+		utils->data, vio_en_gpio_name, 0);
+	if (!gpio_is_valid(panel->reset_config.vio_en_gpio))
+		DSI_DEBUG("vio_en_gpio gpio not specified\n");
+
+	panel->reset_config.vci_en_gpio = utils->get_named_gpio(
+		utils->data, vci_en_gpio_name, 0);
+	if (!gpio_is_valid(panel->reset_config.vci_en_gpio))
+		DSI_DEBUG("vci_en_gpio gpio not specified\n");
 
 	data = utils->get_property(utils->data,
 		"qcom,mdss-dsi-mode-sel-gpio-state", NULL);

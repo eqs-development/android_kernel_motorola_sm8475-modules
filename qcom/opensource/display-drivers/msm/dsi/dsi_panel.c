5b06a979ed7c3cad7b9f0e7871052e828587707d
houdz1 <houdz1@lenovo.com>
2022-08-12 16:19:48 +0800

    dsi_panel: fix the bug of local HBM
    1.When enable local HBM ,then we will use "dbv_level".
    2.Dynamically modify some commands, t
    hen parse the next commands(cmds++)
    
    Change-Id: Ic54a21240a7f9672657452432f34513d4f4e6cac
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2361625
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -1186,7 +1186,7 @@ static int dsi_panel_set_local_hbm_param(struct dsi_panel *panel,
 		}else
 			hbm_bl_lvl = lhbm_config->dbv_level;
 
-		for (i =0; i < count; i++) {
+		for (i =0; i < count; i++,cmds++) {
 			payload = (u8 *)cmds->msg.tx_buf;
 			if(param_info->value == HBM_FOD_ON_STATE) {
 				if(payload[0] == lhbm_config->alpha_reg) {
@@ -1226,7 +1226,6 @@ static int dsi_panel_set_local_hbm_param(struct dsi_panel *panel,
 				rc =  0;
 				goto end;
 			}
-			cmds++;
 		}
 	}
 
@@ -1261,7 +1260,7 @@ static int dsi_panel_set_hbm(struct dsi_panel *panel,
 		else
 			bl_lvl = HBM_BRIGHTNESS(param_info->value);
 		mutex_lock(&panel->panel_lock);
-		rc = dsi_panel_set_backlight(panel, HBM_BRIGHTNESS(param_info->value));
+		rc = dsi_panel_set_backlight(panel, bl_lvl);
 		mutex_unlock(&panel->panel_lock);
 		if (rc)
 			DSI_ERR("unable to set backlight\n");

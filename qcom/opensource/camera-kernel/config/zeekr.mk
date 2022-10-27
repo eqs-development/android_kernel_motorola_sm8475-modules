a108f0dbd71b798e8babb31b0580fb9acc7afd19
wangshuo18 <wangshuo18@lenovo.com>
2022-10-27 15:18:12 +0800

    Juno: remove MMI_STOPSHIP for user build
    -remove MMI_STOPSHIP flag cuz it will lead to build waring for all product.
    -remove cci tool and cci dump for juno user build.
    
    Change-Id: I4f304a1de0c1b8e866628c860c4a77aeb470457c
    Signed-off-by: wangshuo18 <wangshuo18@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2414798
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: <huqian4@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/camera-kernel/config/zeekr.mk
+++ qcom/opensource/camera-kernel/config/zeekr.mk
@@ -1,11 +1,14 @@
 # Settings for compiling waipio camera architecture
 
 # Localized KCONFIG settings
-# MMI_STOPSHIP Camera: Remove for user build
+# Camera: Remove for user build
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 CONFIG_CCI_DEBUG_INTF := y
+ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
+endif
+
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
 # Flags to pass into C preprocessor
-ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
 ccflags-y += -DCONFIG_MOT_OIS_AFTER_SALES_SERVICE=1

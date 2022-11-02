25508deb65257ced35fac3e6529c929ff2e6b700
wangshuo18 <wangshuo18@lenovo.com>
2022-11-02 10:29:15 +0800

    camera: remove MMI_STOPSHIP for all product
    -remove MMI_STOPSHIP to avoid build warnings
    -only apply CCI_DEBUG_INTF on userdebug/eng build
    
    Change-Id: I50a18e84869d3403aeeebbec29b6960d87393486
    Signed-off-by: wangshuo18 <wangshuo18@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2418402
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/camera-kernel/config/li.mk
+++ qcom/opensource/camera-kernel/config/li.mk
@@ -1,10 +1,12 @@
 # Settings for compiling waipio camera architecture
 
 # Localized KCONFIG settings
-# MMI_STOPSHIP Camera: Remove for user build
+# Camera: Remove for user build
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 CONFIG_CCI_DEBUG_INTF := y
+ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
+endif
 CONFIG_CCI_ADDR_SWITCH := y
 
 # Flags to pass into C preprocessor
-ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 ccflags-y += -DCONFIG_CCI_ADDR_SWITCH=1

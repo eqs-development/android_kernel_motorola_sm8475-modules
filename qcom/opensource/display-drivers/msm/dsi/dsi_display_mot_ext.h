388725dd90bff4717b8dc8e13c1772da28a62b1a
daiss1 <daiss1@lenovo.com>
2022-12-20 17:09:42 +0800

    msm/drm: Juno According to the basic refresh rate, switch 30/10/1hz
    Juno According to the basic refresh rate, switch 30/10/1hz
    30/10/1hz-->
    165/120/90/60/24/ base 120hz
    90 base 90hz
    
    Change-Id: I73bb86fab4d13f3b027b00a2cd32659c53986625
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2466248
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dsi/dsi_display_mot_ext.h
+++ qcom/opensource/display-drivers/msm/dsi/dsi_display_mot_ext.h
@@ -76,6 +76,7 @@ static inline u32 dsi_display_mode_actual_rr(struct dsi_mode_info *timing)
 	else
 		return timing->refresh_rate;
 }
+void mot_swtich_base(struct dsi_display *display, u32 refresh_rate);
 // END Motorola zhanggb, IKSWT-18219
 
 #endif /* _DSI_DISPLAY_MOT_EXT_H_ */

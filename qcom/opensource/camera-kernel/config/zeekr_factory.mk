f0015e947bc5cf0305c9ce7feb2109669cfd94b4
wangshuo18 <wangshuo18@lenovo.com>
2023-01-03 10:37:18 +0800

    zeekr: enable AF_NOISE_ELIMINATION on factory build
    -enable AF_NOISE_ELIMINATION on factory build
    -enable vibrator&camera resonance issue SW workaround
    
    Change-Id: I944c9a827178f385ef71339f2762f0ac8d6b9719
    Signed-off-by: wangshuo18 <wangshuo18@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2475194
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: XiaoFeng Wang <wangxf32@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/camera-kernel/config/zeekr_factory.mk
+++ qcom/opensource/camera-kernel/config/zeekr_factory.mk
@@ -8,7 +8,9 @@ ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 endif
 
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
+CONFIG_AF_NOISE_ELIMINATION := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
 ccflags-y += -DCONFIG_MOT_OIS_AFTER_SALES_SERVICE=1
+ccflags-y += -DCONFIG_AF_NOISE_ELIMINATION=1

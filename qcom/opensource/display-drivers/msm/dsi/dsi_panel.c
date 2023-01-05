6608d085677a23137810ef5c55256ee03dccc789
houdz1 <houdz1@lenovo.com>
2023-01-05 16:20:44 +0800

    dsi_panel: update the HBM commands according to FPS
    For Bronco's 1st panel, must update the HBM commands according to FPS.
    When enter and exit HBM mode(not FOD HBM  mode),
    if  fps is 90, write 0x62 to 01.
    For other fps value , write 0x62 to 00.
    
    Change-Id: Iaf001c7718f6daa00200ffbdf41d58b2865d8a13
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2477723
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -1383,6 +1383,38 @@ static int dsi_panel_lhbm_waitfor_fps_valid(struct dsi_panel *panel)
 	return 0;
 }
 
+void update_hbm_cmd_bronco(struct dsi_panel *panel,
+                        struct msm_param_info *param_info)
+{
+	struct panel_param_val_map *param_map;
+	struct panel_param_val_map *param_map_state;
+	struct dsi_cmd_desc *cmds;
+	u32 i;
+	u32 count;
+	u8 *payload;
+	param_map = panel->param_cmds[param_info->param_idx].val_map;
+	param_map_state = &param_map[param_info->value];
+	if (!param_map_state->cmds || !param_map_state->cmds->cmds) {
+		DSI_ERR("%s:Invalid cmds or cmds->cmds\n", __func__);
+		return;
+	}
+
+	cmds = param_map_state->cmds->cmds;
+	count = param_map_state->cmds->count;
+
+	mutex_lock(&panel->panel_lock);
+
+	for ( i =0; i < count; i++,cmds++) {
+		payload = (u8 *)cmds->msg.tx_buf;
+		if (payload[0] == 0X62) {
+			if(panel->cur_mode->timing.refresh_rate == 90) payload[1] = 0x01;
+			else payload[1] = 0x00;
+			pr_info("%s: payload[0] = 0x%x payload[1] = 0x%x", __func__, payload[0],payload[1]);
+		}
+	}
+	mutex_unlock(&panel->panel_lock);
+}
+
 static int dsi_panel_set_hbm(struct dsi_panel *panel,
                         struct msm_param_info *param_info)
 {
@@ -1393,6 +1425,9 @@ static int dsi_panel_set_hbm(struct dsi_panel *panel,
 	pr_info("Set HBM to (%d)\n", param_info->value);
 	if(panel->delect_dc_onoff && param_info->value==0)
 		mot_update_hbmoff(panel, param_info);
+
+	if(panel->hbm_detect_fps && param_info->value != HBM_FOD_ON_STATE)
+		update_hbm_cmd_bronco(panel, param_info);
 
 	if(param_info->value == HBM_FOD_ON_STATE) msleep(20);
 
@@ -4918,6 +4953,10 @@ static int dsi_panel_parse_mot_panel_config(struct dsi_panel *panel,
        }
 	panel->delect_dc_onoff = of_property_read_bool(of_node,
 				"qcom,delect_dc_onoff");
+
+	panel->hbm_detect_fps = of_property_read_bool(of_node,
+				"qcom,mdss-dsi-hbm-detect-fps");
+
 
 	panel->mot_nt37701A_read_cellid = of_property_read_bool(of_node,
 				"qcom,mot_nt37701A_read_cellid");

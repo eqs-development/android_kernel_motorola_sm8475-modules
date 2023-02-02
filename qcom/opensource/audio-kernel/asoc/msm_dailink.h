ee706fb6fcaf9d35c8d661a54b95ca357135c04b
zhouwl <zhouwl@lenovo.com>
2023-02-02 11:17:57 +0800

    ASoC: genevn: rename aw882xx dailink name
    Aw882xx dailink name is aw882xx-aif-0-34, aw882xx-aif-0-35.
    In some cases, aw882xx probes i2c bus number is be changed,
    causes snd card register failure.
    Enable rename-flag to change dailink name, aw882xx v1.13.0
    driver supports it.
    
    Change-Id: If58b22d9f8ef3c12cbdfae00edb8f564f2791635
    Signed-off-by: zhouwl <zhouwl@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2500284
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/audio-kernel/asoc/msm_dailink.h
+++ qcom/opensource/audio-kernel/asoc/msm_dailink.h
@@ -466,13 +466,13 @@ SND_SOC_DAILINK_DEFS(tert_mi2s_tx_reed,
 
 SND_SOC_DAILINK_DEFS(quin_mi2s_rx_aw882xx,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa.0-0034", "aw882xx-aif-0-34"),
-	                   COMP_CODEC("aw882xx_smartpa.0-0035", "aw882xx-aif-0-35")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa_0", "aw882xx-aif-0"),
+	                   COMP_CODEC("aw882xx_smartpa_1", "aw882xx-aif-1")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
 SND_SOC_DAILINK_DEFS(quin_mi2s_tx_aw882xx,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa.0-0034", "aw882xx-aif-0-34"),
-	                   COMP_CODEC("aw882xx_smartpa.0-0035", "aw882xx-aif-0-35")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa_0", "aw882xx-aif-0"),
+	                   COMP_CODEC("aw882xx_smartpa_1", "aw882xx-aif-1")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 

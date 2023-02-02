b82ff87333fb8837efba93f2ddbe1b354ddae435
Cosmin Tanislav <demonsingur@gmail.com>
2023-02-02 02:20:43 +0200

    display: apply HBM status on panel post enable
    Change-Id: I43a0fd3f5aa84ae60fd0ee2a1301a89e59a79f8a

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -5045,6 +5045,7 @@ static ssize_t dsi_panel_set_hbm_status(struct dsi_panel *panel, bool hbm_status
 										bool fodhbm_status)
 {
 	struct msm_param_info param_info;
+
 	param_info.param_idx = PARAM_HBM_ID;
 	if(hbm_status)
 		param_info.value = HBM_ON_STATE;
@@ -5120,7 +5121,6 @@ static ssize_t sysfs_fod_hbm_write(struct device *dev,
 {
 	struct dsi_display *display = dev_get_drvdata(dev);
 	struct dsi_panel *panel = display->panel;
-	struct msm_param_info param_info;
 	bool status;
 	int rc;
 
@@ -6782,6 +6782,15 @@ int dsi_panel_post_enable(struct dsi_panel *panel)
 			pr_err("[%s] failed to send DSI_CMD_SET_ON cmds, rc=%d\n",
 			       panel->name, rc);
 		}
+	}
+
+	if (panel->hbm_enabled) {
+	    mutex_unlock(&panel->panel_lock);
+		rc = dsi_panel_set_hbm_status(panel, false, false);
+		rc = dsi_panel_set_hbm_status(panel, panel->hbm_enabled,
+					panel->fod_hbm_enabled);
+		if (rc)
+			goto error;
 	}
 
 	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_ON);

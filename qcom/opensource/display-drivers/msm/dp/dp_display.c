0a44e58eb371b3fc3c9b0269e8d277558578c5eb
Guobin Zhang <zhanggb@motorola.com>
2023-02-21 15:08:00 +0800

    DP: switch on aux if dp is disconnected by hpd_low
    Usually DP device will sent hpd_high 0 with pin=0 when DP device dissconnects.
    Some DP devices may sent hpd_high 0 then hpd_high 1 to reset itself for
    some reason. But the value of pin in these process is always none-zero,
    which cause Configure cb cannot be called thus Aux can not be switch on,
    Aux transfer then failed.
    
    Change-Id: I06eb44b8e4396eb55e3a935114ccd0f77fb5fcac
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2517509
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Chuwei Xu <xucw@motorola.com>

--- qcom/opensource/display-drivers/msm/dp/dp_display.c
+++ qcom/opensource/display-drivers/msm/dp/dp_display.c
@@ -1949,6 +1949,12 @@ static void dp_display_connect_work(struct work_struct *work)
 		return;
 	}
 
+	if (dp_display_state_is(DP_STATE_DISCONNECT_NOTIFIED) && dp->hpd->hpd_high) {
+		if (!dp->debug->sim_mode && !dp->parser->no_aux_switch && !dp->parser->gpio_aux_switch) {
+			dp->aux->aux_switch(dp->aux, true, dp->hpd->orientation);
+		}
+	}
+
 	rc = dp_display_process_hpd_high(dp);
 
 	if (!rc && dp->panel->video_test)

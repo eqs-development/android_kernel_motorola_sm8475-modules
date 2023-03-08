11ea4c8647523a309549935be057784cf0a987e0
wangdp11 <wangdp11@motorola.com>
2023-03-08 20:52:20 +0800

    lynkco: enable AF_NOISE_ELIMINATION for UW[2/2]
    Details:
    1.Enable the macro for resonance noise between DW9800 and phone
    vibrator.
    2.The safe DAC position is 0.(comes from KP)
    
    Change-Id: I203812b391358c092915cc3d2c7b955c401be4bc
    Signed-off-by: wangdp11 <wangdp11@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2533021
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/camera-kernel/config/lynkco.mk
+++ qcom/opensource/camera-kernel/config/lynkco.mk
@@ -7,6 +7,8 @@ CONFIG_CCI_DEBUG_INTF := y
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 endif
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
+CONFIG_AF_NOISE_ELIMINATION := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
+ccflags-y += -DCONFIG_AF_NOISE_ELIMINATION=1

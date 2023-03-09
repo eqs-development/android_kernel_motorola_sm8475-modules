f298f1c20c50f76bcb64978a0f4ba18213d488e1
daiss1 <daiss1@lenovo.com>
2023-03-09 13:51:26 +0800

    msm/dsi: Modify DC commands according to the refresh rate
    Dynamic modification of DC commands according to the refresh rate
    Nt37705 DC on/off 6D 00 00 dynamic settings
    
    Change-Id: Ie315da1eee373d7297d3e2648f7c38e53550ab8e
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2533892
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dsi/dsi_display_mot_ext.h
+++ qcom/opensource/display-drivers/msm/dsi/dsi_display_mot_ext.h
@@ -77,5 +77,7 @@ static inline u32 dsi_display_mode_actual_rr(struct dsi_mode_info *timing)
 }
 void mot_swtich_base(struct dsi_display *display, u32 refresh_rate);
 // END Motorola zhanggb, IKSWT-18219
+void update_dc_cmd_nt37705(struct dsi_panel *panel,
+                        struct panel_param_val_map *param_map_state);
 
 #endif /* _DSI_DISPLAY_MOT_EXT_H_ */

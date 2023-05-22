a1bc295091750ba88f0f8b0d985238fad4357bad
daiss1 <daiss1@lenovo.com>
2023-05-22 14:39:32 +0800

    msm/dsi: Add acquire panel lock when read 8s code
    Modify:
    Add dsi_panel_acquire_panel_lock,
    Add acquire panel lock while sending 8s code command
    
    Change-Id: I5e574e0e11c14f4e91658c978f144893cb87f829
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2616322
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -6705,6 +6705,7 @@ int dsi_panel_tx_cellid_cmd(struct dsi_panel *panel)
 		goto error;
 	}
 
+	dsi_panel_acquire_panel_lock(panel);
 	for (i = 0; i < count; i++) {
 		cmds->ctrl_flags = 0;
 
@@ -6731,6 +6732,7 @@ int dsi_panel_tx_cellid_cmd(struct dsi_panel *panel)
 		cmds++;
 	}
 error:
+	dsi_panel_release_panel_lock(panel);
 	return rc;
 }
 

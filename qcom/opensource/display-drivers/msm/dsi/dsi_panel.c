4f59c2a31b76c2fc886259b4783f0aabd4a88708
SGCMarkus <markusornik@gmail.com>
2023-09-14 22:08:44 +0200

    display: remove mutex from panel feature sysfs
    mutex lock happens later down the chain

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -5023,7 +5023,6 @@ static ssize_t sysfs_hbm_write(struct device *dev,
 	if (rc)
 		return rc;
 
-	mutex_lock(&panel->panel_lock);
 	if (panel->hbm_enabled == status)
 		goto exit;
 
@@ -5037,8 +5036,6 @@ static ssize_t sysfs_hbm_write(struct device *dev,
 	panel->hbm_enabled = status;
 
 exit:
-	mutex_unlock(&panel->panel_lock);
-
 	return rc ?: count;
 }
 
@@ -5073,7 +5070,6 @@ static ssize_t sysfs_fod_hbm_write(struct device *dev,
 	if (rc)
 		return rc;
 
-	mutex_lock(&panel->panel_lock);
 	if (panel->fod_hbm_enabled == status)
 		goto exit;
 
@@ -5087,8 +5083,6 @@ static ssize_t sysfs_fod_hbm_write(struct device *dev,
 	panel->fod_hbm_enabled = status;
 
 exit:
-	mutex_unlock(&panel->panel_lock);
-
 	return rc ?: count;
 }
 
@@ -5123,7 +5117,6 @@ static ssize_t sysfs_dc_dimming_write(struct device *dev,
 	if (rc)
 		return rc;
 
-	mutex_lock(&panel->panel_lock);
 	if (panel->dc_state == status)
 		goto exit;
 
@@ -5135,8 +5128,6 @@ static ssize_t sysfs_dc_dimming_write(struct device *dev,
 		goto exit;
 
 exit:
-	mutex_unlock(&panel->panel_lock);
-
 	return rc ?: count;
 }
 

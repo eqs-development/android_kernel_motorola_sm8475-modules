4c5e43c70521f6eac05ac5242675f903bcf3d121
yangmf3 <yangmf3@motorola.com>
2023-09-26 20:02:56 +0800

    boston: enable CONFIG_CAM_SENSOR_PROBE_DEBUG
    details:
    enable CONFIG_CAM_SENSOR_PROBE_DEBUG for boston
    
    Change-Id: Ia9bb4a868adf6148518bcfec6bd0ee764c00dcb7
    Signed-off-by: yangmf3 <yangmf3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2752050
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: <hezheng1@motorola.com>
    Reviewed-by: Bolei Shang <shangbl1@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/camera-kernel/config/boston_factory.mk
+++ qcom/opensource/camera-kernel/config/boston_factory.mk
@@ -6,5 +6,7 @@ ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 CONFIG_CCI_DEBUG_INTF := y
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 endif
+CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
 # Flags to pass into C preprocessor
+ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1

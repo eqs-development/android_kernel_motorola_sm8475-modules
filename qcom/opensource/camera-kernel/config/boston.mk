550325dc74666e09d9b360ec96d27455130699bb
zuoyuan <zuoyuan1@motorola.com>
2023-10-18 19:41:40 +0800

    [Boston]Power on and initialize sensor in advance[1/2]
    [detail]
    1.add power on and initialize sensor from zeekr to boston
    2.add PRE_POWERON_SENSOR compline macro definition to boston.mk
    
    Change-Id: Ic3eeb6c9ed036e9ea23fb9f60c6300d1ea103f1c
    Signed-off-by: zuoyuan <zuoyuan1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2771467
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <zhanys2@motorola.com>
    Reviewed-by: Darong Huang <huangdra@motorola.com>
    Reviewed-by: <leixu1@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/camera-kernel/config/boston.mk
+++ qcom/opensource/camera-kernel/config/boston.mk
@@ -7,6 +7,8 @@ CONFIG_CCI_DEBUG_INTF := y
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 endif
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
+CONFIG_MOT_SENSOR_PRE_POWERUP := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
+ccflags-y += -DCONFIG_MOT_SENSOR_PRE_POWERUP=1

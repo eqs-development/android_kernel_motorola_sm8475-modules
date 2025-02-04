c68ac8c09850804211ed5a9d6f7747551b23af1f
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-04 14:01:53 +0200

    Remove support for panel notifications
    Change-Id: Id7a6baac11798aee14a11053c839a6dc1b332188

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -12,17 +12,6 @@
 #include <video/mipi_display.h>
 #include <misc/isl97900_led.h>
 
-#if defined(CONFIG_PANEL_NOTIFICATIONS)
-#include <linux/panel_notifier.h>
-
-#define PANEL_NOTIFY(note) { \
-	int idx = dsi_panel_get_index(panel); \
-	panel_notify(note, (void *)&idx); \
-}
-#else
-#define PANEL_NOTIFY(...)
-#endif
-
 #include "dsi_display.h"
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
@@ -5900,19 +5889,6 @@ int dsi_panel_post_switch(struct dsi_panel *panel)
 	return rc;
 }
 
-#if defined(CONFIG_PANEL_NOTIFICATIONS)
-static inline int dsi_panel_get_index(struct dsi_panel *panel)
-{
-	struct dsi_display *dsi_display =
-			container_of(panel->host, struct dsi_display, host);
-
-	if (unlikely(dsi_display == NULL))
-		return -EINVAL;
-
-	return dsi_display->display_idx;
-}
-#endif
-
 int dsi_panel_enable(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -5952,7 +5928,6 @@ int dsi_panel_enable(struct dsi_panel *panel)
 
 	panel->panel_initialized = true;
 
-	PANEL_NOTIFY(PANEL_EVENT_PRE_DISPLAY_ON);
 error:
 	mutex_unlock(&panel->panel_lock);
 
@@ -5990,8 +5965,6 @@ int dsi_panel_post_enable(struct dsi_panel *panel)
 		goto error;
 	}
 
-	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_ON);
-
 error:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -6017,8 +5990,6 @@ int dsi_panel_pre_disable(struct dsi_panel *panel)
 		       panel->name, rc);
 		goto error;
 	}
-
-	PANEL_NOTIFY(PANEL_EVENT_PRE_DISPLAY_OFF);
 
 error:
 	mutex_unlock(&panel->panel_lock);
@@ -6062,8 +6033,6 @@ int dsi_panel_disable(struct dsi_panel *panel)
 	}
 	panel->panel_initialized = false;
 	panel->power_mode = SDE_MODE_DPMS_OFF;
-
-	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_OFF);
 
 	mutex_unlock(&panel->panel_lock);
 	return rc;
